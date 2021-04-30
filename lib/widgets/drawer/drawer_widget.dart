e60af51776c62d04cc501c22883c67f94aead4b9
rafaelofficial <rafael.sousa.pereira@mail.usf.edu.br>
2021-04-30 03:04:39 -0300

    componentizando o drawer e movendo a pasta home para pasta widget

---
+++ lib/widgets/drawer/drawer_widget.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:poke_app/core/core.dart';
+import 'package:poke_app/utils/consts.dart';
+
+class DrawerWidget extends StatefulWidget {
+  @override
+  _DrawerWidgetState createState() => _DrawerWidgetState();
+}
+
+class _DrawerWidgetState extends State<DrawerWidget> {
+  final String titulo = "Poke App";
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Drawer(
+        child: ListView(
+          children: <Widget>[
+            DrawerHeader(
+              child: Text(
+                titulo,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18.0,
+                  color: AppColors.white,
+                ),
+              ),
+              decoration: BoxDecoration(color: AppConsts.secundaryColor),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.home,
+                size: 35.0,
+                color: AppColors.black,
+              ),
+              title: Text(
+                'Home',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18.0,
+                  color: AppColors.black,
+                ),
+              ),
+              onTap: () {},
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
