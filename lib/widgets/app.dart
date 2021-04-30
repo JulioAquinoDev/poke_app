fbe94b97514e5fb361ee121c040be877288a91ab
rafaelofficial <rafael.sousa.pereira@mail.usf.edu.br>
2021-04-30 03:07:41 -0300

    componentizando o drawer

--- lib/widgets/app.dart
+++ lib/widgets/app.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:poke_app/core/core.dart';
 import 'package:poke_app/utils/consts.dart';
 import 'package:poke_app/widgets/home/home.dart';
 
+import 'drawer/drawer_widget.dart';
+
 class App extends StatelessWidget {
-  final String titulo = 'Pokémon App';
+  final String titulo = 'Poke App';
 
   @override
   Widget build(BuildContext context) {
@@ -22,39 +23,7 @@ class App extends StatelessWidget {
         // Cor de fundo amarelho
         backgroundColor: AppConsts.primaryColor,
         body: HomePage(),
-        drawer: Drawer(
-          child: ListView(
-            children: <Widget>[
-              DrawerHeader(
-                child: Text(
-                  titulo,
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18.0,
-                    color: AppColors.white,
-                  ),
-                ),
-                decoration: BoxDecoration(color: AppConsts.secundaryColor),
-              ),
-              ListTile(
-                leading: Icon(
-                  Icons.home,
-                  size: 35.0,
-                  color: AppColors.black,
-                ),
-                title: Text(
-                  'Home',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18.0,
-                    color: AppColors.black,
-                  ),
-                ),
-                onTap: () {},
-              ),
-            ],
-          ),
-        ),
+        drawer: DrawerWidget(),
       ),
     );
   }
