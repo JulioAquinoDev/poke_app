04c4c2d3751448a3847a62a97ee2b0977e031f70
rafaelofficial <rafael.sousa.pereira@mail.usf.edu.br>
2021-05-01 01:23:00 -0300

    criado a tela de pesquisas

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 //import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-import 'widgets/app.dart';
+import 'widgets/search/search_widget.dart';
 
 void main() {
   // WidgetsFutureBinding.ensureInitialized();
   // await Firebase.initializeApp();
-  runApp(App());
+  runApp(SearchWidget());
 }
