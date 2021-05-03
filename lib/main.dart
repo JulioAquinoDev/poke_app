e4f0d4b10469eab6e72d908da0901a65df7ca527
Júlio Aquino <julioca.camilo@gmail.com>
2021-05-02 21:20:17 -0300

    Atualização

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
-import 'package:firebase_core/firebase_core.dart';
+//import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'widgets/my_widgets.dart';
 
 
 Future<void> main() async {
-  WidgetsFutureBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  //WidgetsFutureBinding.ensureInitialized();
+  //await Firebase.initializeApp();
   runApp(MyWidgetsApp());
 
 } 
