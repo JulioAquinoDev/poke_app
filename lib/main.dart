8ac1e166fd30a65829be5f0b23bc6466c2b550aa
Júlio Aquino <julioca.camilo@gmail.com>
2021-05-09 13:19:11 -0300

    Resolvendo conflitos

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:poke_app/widgets/details/details_widget.dart';
+import 'package:poke_app/widgets/search/search_widget.dart';
 
 void main() {
-<<<<<<< HEAD
-  runApp(DetailsWidget());
-=======
   runApp(SearchWidget());
->>>>>>> ddc3ed6159ffeef22f02fe7b4890411fffc80459
 }
