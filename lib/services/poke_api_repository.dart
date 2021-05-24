15f9b0ea34407fc74ab72387edaac28483b1f163
rafaelofficial <rafael.sousa.pereira@mail.usf.edu.br>
2021-05-24 09:40:54 -0300

    adicionando model repository e services do projeto

--- lib/services/poke_api_repository.dart
+++ lib/services/poke_api_repository.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 
-import 'package:http/http.dart' as service;
-import 'package:pokeapi/pokeapi.dart';
+import 'package:http/http.dart' as http;
 
 import 'package:poke_app/model/poke_model.dart';
 import 'package:poke_app/repository/i_poke_repository.dart';
@@ -9,7 +8,8 @@ import 'package:poke_app/repository/i_poke_repository.dart';
 class PokeApiRepository implements IPokeRepository {
   @override
   Future<List<PokeModel>> findAllPoke() async {
-    final response = await service.get('https://pokeapi.co/api/v2/pokemon/1');
+    var url = Uri.parse('https://pokeapi.co/api/v2/pokemon');
+    final response = await http.get(url);
     final List<Map<String, dynamic>> responseMap = jsonDecode(response.body);
     return responseMap
         .map<PokeModel>((resp) => PokeModel.fromMap(resp))
